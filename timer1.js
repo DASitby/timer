88b5694f5fb8a5721572e748b6c494ed588dc2ab
DASitby <davidsooley551@gmail.com>
2022-12-09 15:44:52 +0000

    timer logic written, it ignores negative numbers and non-numbers

---
+++ timer1.js
@@ -0,0 +1,27 @@
+const cmdargs = process.argv.slice(2);
+
+const timer = (args) => {
+  let results = [];
+  //If no numbers are provided, prompt the user to try again with some numbers and end program
+  if (args.length === 0) {
+    console.log("No timers set, try entering some positive numbers in the command line!");
+    return;
+  }
+  args.forEach((element,index) => {
+    if (!isNaN(element)) {
+      if (element < 0) {
+        console.log(`please enter only positive numbers; removed '${element}'`);
+      } else {
+        results.push(element);
+      }
+    } else {
+      console.log(`please enter only positive numbers; removed '${element}'`);
+    }
+  });
+  results.forEach(element => {
+    setTimeout(()=>process.stdout.write('\x07'), 1000 * element);
+  });
+  console.log(`Timers for ${results.slice(0,results.length - 1)} and ${results[results.length - 1]} set, waiting...`);
+};
+
+timer(cmdargs);
