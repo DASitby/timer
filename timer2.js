8b27ce86b5a876f55a58eb1b29c522f5a632207e
DASitby <davidsooley551@gmail.com>
2022-12-09 16:53:31 +0000

    got the exit mesage working

--- timer2.js
+++ timer2.js
@@ -6,20 +6,27 @@ const rl = readline.createInterface({
 
 const timer = () =>{
   return new Promise((resolve,reject) => {
-    rl.question('Please enter a time in seconds', (time) => {
+    rl.question('Please enter a time in seconds: ', (time) => {
       if (time >= 0) {
         console.log(`Timer set for ${time} seconds`);
         setTimeout(()=>process.stdout.write('\x07'), 1000 * time);
         resolve(setTimeout(timer,1000 * time));
       } else if (time === 'b') {
         process.stdout.write('\x07');
-        resolve(timer);
+        resolve(timer());
+      } else if (time === 'SIGINT') {
+        console.log("Quitting!");
+        process.exit;
       } else {
         timer();
       }
     });
   });
 };
+
+process.on('exit', () => {
+  console.log("\nThanks for using me, ciao!");
+});
 
 const main = async() => {
   await timer();
